dd641953f64cda1d30dfd51d47932cdc6a1b3a06
Quoc Dat <ngoluuquocdat@gmail.com>
2022-04-08 15:31:35 +0700

    add authen service

--- HappyVacation/Program.cs
+++ HappyVacation/Program.cs
@@ -3,7 +3,11 @@ using HappyVacation.Repositories.Authen;
 using HappyVacation.Repositories.Tours;
 using HappyVacation.Services.Storage;
 using HappyVacation.Services.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,11 +19,69 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<MyDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-//services cors
+builder.Services.AddEndpointsApiExplorer();
+
+// add Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "HappyVacationApi",
+        Description = "An ASP.NET Core Back-end API for HappyVacation",
+        Contact = new OpenApiContact
+        {
+            Name = "Contact via Facebook",
+            Url = new Uri("https://www.facebook.com/quocdat.ngoluu/")
+        },
+    });
+    // add authentication header for Swagger
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                          Enter 'Bearer' [space] and then your token in the text input below.
+                          \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
+// add Authentication service
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+
+// services cors
 builder.Services.AddCors(p => p.AddPolicy("MyCorsPolicy", builder =>
 {
     builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
@@ -28,7 +90,7 @@ builder.Services.AddCors(p => p.AddPolicy("MyCorsPolicy", builder =>
 // add my services and repositories
 builder.Services.AddScoped<IStorageService, StorageService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IUserRepository, TourRepository>();
+builder.Services.AddScoped<ITourRepository, TourRepository>();
 builder.Services.AddScoped<IAuthenRepository, AuthenRepository>();
 
 var app = builder.Build();
@@ -46,6 +108,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseCors("MyCorsPolicy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
